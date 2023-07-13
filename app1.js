385510534e99eb7b6a273a8743d8e26ecc276a97
kira <kirabezuglaya@gmail.com>
2023-07-13 20:13:29 +0300

    setInterval() fixed, app is working properly (most likely)

--- app1.js
+++ app1.js
@@ -10,23 +10,24 @@ const list = document.getElementById("list"); //list for interval values
 let timer; //for stopwatch
 let listInterval,
   counter = 0; //for interval speed
+
+let counter2 = 0;
 let isClicked = false; //for button
 
 //for calculating distance
 let pos1 = { lat: 0.0, lon: 0.0 };
 let pos2 = { lat: 0.0, lon: 0.0 };
 
-btnStart.onclick = function () {
+btnStart.onclick = () => {
   if (!isClicked) {
     //Start
     isClicked = true;
     document.querySelector(".button").classList.remove("unclicked");
     document.querySelector(".button").classList.add("clicked");
-    
-    //let intervalValue = interval.value * 60000; //value of minutes in miliseconds
+
     startStopwatch(); //Stopwatch start
-    // debugger
-    startIntervalSpeedList(); //interval speed
+    //debugger
+    startIntervalSpeedList();
   } else {
     //Stop
     isClicked = false;
@@ -89,32 +90,55 @@ function calculateDistance(pos1, pos2) {
 }
 function addInterwalInfo(time = "время", speed = "скорость") {
   let str = time + "   ---   " + speed + " kt";
-  list.innerHTML = `<li>${str}</li>`;
+  //list.innerHTML = `<li>${str}</li>`;
+  list.insertAdjacentHTML("afterbegin", `<li>${str}</li>`);
 }
 function loop() {
-  navigator.geolocation.getCurrentPosition(function (position) {
-    pos2.lat = position.coords.latitude;
-    pos2.lon = position.coords.longitude;
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      pos2.lat = position.coords.latitude;
+      pos2.lon = position.coords.longitude;
 
-    let dist = calculateDistance(pos1, pos2);
-    let time = interval.value / 60;
-    let speed = (dist / time).toFixed(2);
+      let dist = calculateDistance(pos1, pos2);
+      let time = interval.value / 60;
+      let speed = (dist / time).toFixed(2);
 
-    if (counter > 0) {
-      addInterwalInfo(undefined, speed);
-    } else {
-      addInterwalInfo(undefined, undefined);
+      if (counter > 0) {
+        addInterwalInfo(stopwatch.textContent, speed);
+      } else {
+        addInterwalInfo(undefined, undefined);
+      }
+
+      pos1.lat = pos2.lat;
+      pos1.lon = pos2.lon;
+
+      counter++;
+
+      sound();
+    },
+    () => {
+      console.log("i broke");
     }
-
-    pos1.lat = pos2.lat;
-    pos1.lon = pos2.lon;
-
-    counter++;
-  }, ()=>{console.log('i broke')});
+  );
 }
 function startIntervalSpeedList() {
-  listInterval = setInterval(loop(), interval.value * 1000 * 60);
+  listInterval = setInterval(loop, interval.value * 60 * 1000);
 }
 function stopIntervalSpeedList() {
   clearInterval(listInterval);
+}
+function sound() {
+  const oscillator = audioContext.createOscillator();
+  const gainNode = audioContext.createGain();
+  oscillator.connect(gainNode);
+  gainNode.connect(audioContext.destination);
+
+  oscillator.type = "sine";
+  oscillator.frequency.value = 100;
+
+  const now = audioContext.currentTime;
+  gainNode.gain.setValueAtTime(20, now);
+  gainNode.gain.exponentialRampToValueAtTime(0.11, now + 1);
+  oscillator.start(now);
+  oscillator.stop(now + 1);
 }
